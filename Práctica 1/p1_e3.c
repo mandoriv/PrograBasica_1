6c8f7910dea1747825e947bc0b93cd3cea293832
Armando <65095642+mandoriv@users.noreply.github.com>
2021-03-08 11:30:55 -0600

    P1_C3 terminado

---
+++ Práctica 1/p1_e3.c
@@ -0,0 +1,37 @@
+//
+//  p1_e1.c
+//  
+//
+//  Created by Armando on 08/03/21.
+//
+
+/*
+ Una empresa de venta de recambios de automóviles necesita un programa que calcule y muestre el precio de venta en euros de los productos.
+*/
+
+#include <stdio.h>
+
+int main(){
+    // Declaramos las variables que vamos a utilziar
+    float precioCompra;
+    float margen;
+    float precioVenta;
+    float ganancia;
+    
+    // Pedimos que ingrese los valores
+    printf("Ingrese el precio de compra: ");
+    scanf("%f", &precioCompra);
+    printf("Ingrese el margen: ");
+    scanf("%f", &margen);
+    
+    // Calculamos la ganancia
+    ganancia = precioCompra * (margen/100);
+    
+    // Calculamos el precio de venta
+    precioVenta = precioCompra + ganancia;
+    
+    // Mostramos el resultado
+    printf("El producto se debe vender en: %.2f euros\n", precioVenta);
+    
+    return 0;
+}
