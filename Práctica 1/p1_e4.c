608ae7b5f97e246298d785dd0507142c472693d8
Armando <65095642+mandoriv@users.noreply.github.com>
2021-03-08 11:38:59 -0600

    P1_E4 terminado

---
+++ Práctica 1/p1_e4.c
@@ -0,0 +1,27 @@
+//
+//  p1_e1.c
+//  
+//
+//  Created by Armando on 08/03/21.
+//
+
+/*
+ Realizar un programa que pida al usuario un carácter e indique por pantalla sus caracteres anterior y siguiente en la tabla ASCII.
+*/
+
+#include <stdio.h>
+
+int main(){
+    // Declaramos la variable que vamos a utilziar
+    char caracter;
+    
+    // Leemos el caracter
+    printf("Ingrese el caracter: ");
+    scanf("%c", &caracter);
+    
+    // Mostramos el caracter anterior y siguiente
+    printf("anterior: %c\n", caracter-1);
+    printf("siguiente: %c\n", caracter+1);
+    
+    return 0;
+}
